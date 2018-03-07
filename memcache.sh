2ba4cd98bd2369b6b8577680cca5d0bcb12ebd72
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-03-07 15:10:57 +0100

    Update memcache.sh

--- memcache.sh
+++ memcache.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-echo -en "\x00\x00\x00\x00\x00\x01\x00\x00stats\r\n" | nc -q1 -u 194.224.175.75 11211
+echo Uso.: sh memcache.sh IP
+echo -en "\x00\x00\x00\x00\x00\x01\x00\x00stats\r\n" | nc -q1 -u $1 11211
